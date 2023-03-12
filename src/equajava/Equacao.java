762ad40f5c2a1ed766ebfc6d6d6ebe4e5438b2b1
Vinícius Queiroz <euovinq@gmail.com>
2023-03-12 18:39:04 -0300

    Exclusão de uma classe não utilizada

--- src/equajava/Equacao.java
+++ src/equajava/Equacao.java
@@ -6,13 +6,13 @@ public class Equacao {
 		
 		EquaJava eqj = new EquaJava();
 		
-		int x1 = 2;
-		int y1 = 1;
-		int si1 = 3;
+		int x1 = 3;
+		int y1 = 2;
+		int si1 = 6;
 		
-		int x2 = 1;
-		int y2 = 2;
-		int si2 = -1;
+		int x2 = 2;
+		int y2 = 3;
+		int si2 = 5;
 		
 		
 		System.out.println(eqj.equacao(x1, x2, y1, y2, si1, si2));
